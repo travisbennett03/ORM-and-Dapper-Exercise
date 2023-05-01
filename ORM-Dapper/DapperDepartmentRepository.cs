b852785c2ac49ce1803b0e41acf6fc3d1c4df839
travisbennett03 <tbennett1996@yahoo.com>
2023-05-01 11:58:09 -0500

    initial commit

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            //Constructor
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            var depos = _connection.Query<Department>("SELECT * FROM departments");
+
+            return depos;
+        }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+            new { departmentName = newDepartmentName });
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Drawing.Printing;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            #endregion
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperProductRepository(conn);
+
+            var products = repo.GetAllProducts();
+
+            foreach(var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID} {prod.Name}");
+            }
+
+            DapperDepartmentRepository repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Hello user, here are the current departments:");
+            Console.WriteLine("Plese press enter . . .");
+            Console.ReadLine();
+            var depos = repo.GetAllDepartments();
+            
+           
+
+            Console.WriteLine("Do you want to add a department??");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse.ToLower() == "yes")
+            {
+                Console.WriteLine("What is the name of your new Department??");
+                userResponse = Console.ReadLine();
+
+
+                repo.InsertDepartment(userResponse);
+                Print(repo.GetAllDepartments());
+            }
+
+            Console.WriteLine("Have a great day.");
+          
+        }
+
+        private static void Print(IEnumerable<Department> depos)
+        {
+            foreach (var depo in depos)
+            {
+                Console.WriteLine($"Id: {depo.DepartmentId} Name: {depo.Name}");
+            }
         }
     }
 }
